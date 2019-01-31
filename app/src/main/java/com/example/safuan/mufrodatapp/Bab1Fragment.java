cdc23a5218b2ad9c3e5b7b5a20e8cb667a19864e
asmasyafira <syafirasma@gmail.com>
2019-01-31 10:18:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/safuan/mufrodatapp/Bab1Fragment.java
@@ -0,0 +1,29 @@
+package com.example.safuan.mufrodatapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Bab1Fragment extends Fragment {
+
+
+    public Bab1Fragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bab1, container, false);
+    }
+
+
+}
